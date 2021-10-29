35ee469d8a8f30613709cb9f331a6df96187c6bc
johansen98 <ron.johansen@stud.noroff.no>
2021-10-29 17:44:55 +0200

    added everything
    i dint know that we had to use this repo so i opend a newone so thats why all of the code is beeing uploaded at  once

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function openMenu() {
+    document.getElementById("mySideMenu").style.width = "250px";
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function closeMenu() {
+    document.getElementById("mySideMenu").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log('Thanks for checking out my page and thanks for looking at the code ;D');
