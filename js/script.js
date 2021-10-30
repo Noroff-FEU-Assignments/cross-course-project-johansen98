82985a21d652eae7fed19277bf0c973e1b8e278c
johansen98 <ron.johansen@stud.noroff.no>
2021-10-30 23:57:18 +0200

    updates on js, htm and css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let count = 0;
+// ↓↓↓ desktop menu ↓↓↓
 function openMenu() {
     document.getElementById("mySideMenu").style.width = "400px";
     document.getElementById("overlay").style.display = "block";
@@ -6,10 +8,15 @@ function openMenu() {
   function closeMenu() {
     document.getElementById("mySideMenu").style.width = "0";
     document.getElementById("overlay").style.display = "none";
+
   }
 
+
+
+// ↓↓↓ mobile menu ↓↓↓
+
   function openMobileMenu() {
-    document.getElementById("myMobileSideMenu").style.width = "100%";
+    document.getElementById("myMobileSideMenu").style.width = "100vw";
     
   }
   
@@ -18,6 +25,23 @@ function openMenu() {
     
   }
 
+  function search(){
+    let elm = document.getElementById("searchbar"); 
+    if(elm.style.display === 'block')
+        elm.style.display = 'none'
+    else
+        elm.style.display = 'block'
+
+        if(count === 0){
+        elm.addEventListener("keyup" , function(event) {
+          if(event.keyCode === 13){
+            event.preventDefault();
+            window.location.href="index.html"
+          }
+        })
+      }
+      count++;
+  }
 
 
 
@@ -27,4 +51,9 @@ function openMenu() {
 
 
 
-  console.log('Thanks for checking out my page and thanks for looking at the code ;D');
+
+// ↓↓↓ for the people who are checking console and my code :D ↓↓↓
+
+const style = 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)';
+
+console.log('%c Thanks for checking out my page and thanks for looking at the code ;D' , style);
