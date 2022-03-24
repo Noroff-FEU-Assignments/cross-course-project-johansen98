73e50d3a5fc6abf871902c749408aac5d2b30249
johansen98 <61602808+johansen98@users.noreply.github.com>
2022-03-24 20:26:02 +0100

    UPDATE3

--- js/cartCtrl.js
+++ js/cartCtrl.js
@@ -7,34 +7,58 @@ for(i = 0; i < results.length; i++){
   const orderLineAmountElm = document.createElement("h3")
   const orderLineTitleElm = document.createElement("h3")
   const orderLinePriceElm = document.createElement("h3")
+  const totalSum = document.createElement("h3");
   const sumOrderLine = document.createElement("h3");
   const removeLine = document.createElement("button")
   const addLine = document.createElement("button")
+  const deleteAllBtn = document.getElementById("deleteAllBtn")
+  let total = 0;
+
+  results.forEach(result => {
+    total += result.price * result.amount
+  });
+  console.log("Sum:", total)
 
   removeLine.innerHTML = "-"
   addLine.innerHTML = "+"
   removeLine.style.background = "transparent";
   addLine.style.background = "transparent";
-  
   orderLineAmountElm.innerHTML = result.amount
   orderLinePriceElm.innerHTML = result.price + ",-"
-  orderLineTitleElm.innerHTML = result.title
+  orderLineTitleElm.innerHTML = result.title;
+  totalSum.innerHTML = "Sum: " + total;
+
+  
   sumOrderLine.innerHTML = result.amount * result.price + ",-";
+
+
 
 
   addLine.classList.add("cartItemBtn");
   removeLine.classList.add("cartItemBtn");
   orderLineElm.classList.add("cartItem");
-  
+  totalSum.classList.add("total")
   cartContainer.appendChild(orderLineElm)
   orderLineElm.appendChild(orderLineTitleElm)
+  
   orderLineElm.appendChild(orderLineAmountElm)
   orderLineElm.appendChild(orderLinePriceElm)
   orderLineElm.appendChild(sumOrderLine)
   orderLineElm.appendChild(removeLine)
   orderLineElm.appendChild(addLine)
+  orderLineElm.appendChild(totalSum)
 
-}
-removeItem(){
-    
+
+  function deliteAll(){
+  localStorage.clear('cart');         
+  
+  }
+
+  if(results < 0){
+    deleteAllBtn.style.display = "none";
+  }else{
+    deleteAllBtn.style.display = "block";
+  }
+  
+
 }
--- js/validate.js
+++ js/validate.js
@@ -2,7 +2,9 @@ const fullName = document.getElementById('name');
 const email = document.getElementById('email');
 const subject = document.getElementById('message');
 const forms = document.getElementById('form')
+const checkOutForm = document.getElementById('checkOutForm');
 const msg = document.getElementById('msg');
+
 
 function validateEmail(email) {
     const regEx = /\S+@\S+\.\S+/;
@@ -13,21 +15,23 @@ function validateEmail(email) {
     forms.addEventListener('submit', (a) => {
         let message = []
     
-        if(subject.value.length < 10){
-            message.push('message must have a minimum length of 10 characters')
-            
-        }
-    
         if(!validateEmail(email.value)){
             message.push('E-mail must have a value and be formatted like an email address');
            
         }
+        if(subject.value.length < 10){
+            message.push('Message must have a minimum length of 10 characters')
+            
+        }
+    
         
         if(message.length > 0){
             msg.innerHTML = message.join(' </br> ')
             msg.style.color = 'red';
             
-        } else {
+        }
+
+        else {
             msg.innerText = "Thank you for cotacting us. You will hear from us soon. Have a great day.";
             msg.style.color = 'green';
             
@@ -39,4 +43,7 @@ function validateEmail(email) {
         a.preventDefault()
     
     })
-        
+
+
+
+ 
